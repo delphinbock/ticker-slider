d8381acef84c53365d8e6d1af3ae7a580c80c27a
Delphin Bock <delphin@DROSERA>
2024-07-17 17:36:39 +0200

    Add legacy files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import TickerSlider from "./ticker_slider";
+
+export default function App() {
+  return (
+    <div id="root" className="App">
+      <TickerSlider />
+    </div>
+  );
+}
